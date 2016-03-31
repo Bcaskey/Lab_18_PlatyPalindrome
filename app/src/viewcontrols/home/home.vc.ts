83aae1e1d8e0f87455bd47fda019c104c0cebff8
Bcaskey <bcaskeyu@charter.net>
2016-03-30 19:11:01 -0500

    update for functionaliy.

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -4,7 +4,17 @@ import BaseViewControl from '../base/base.vc';
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {};
-}
+    context: any = {
+        word: '',
+    };
+    
+    toTest(word: string): void {
+        this.navigator.navigate('test-vc', {
+            parameters: {
+                word: word
+            }
+        });
+    }
+}  
 
 register.viewControl('home-vc', HomeViewControl);
--- app/src/viewcontrols/test/test.vc.ts
+++ app/src/viewcontrols/test/test.vc.ts
@@ -4,7 +4,31 @@ import BaseViewControl from '../base/base.vc';
 export default class TestViewControl extends BaseViewControl {
     templateString: string = require('./test.vc.html');
 
-    context: any = {};
+    context: any = {
+        word: '',
+        lwrWord: '',
+        revWord: '',
+        testResults: '',
+        // showMeResult: False
+    };
+    
+    navigatedTo(parameters: any){
+        let newWord = parameters.word;
+        let lwrWord = newWord.toLowerCase();
+        let revWord = '';
+        for (var i = lwrWord.length - 1; i >= 0; i--) {
+            revWord += lwrWord[i];
+        }
+        let testResults = '';
+        if (lwrWord === revWord) {
+            testResults = 'a palindrome.';
+        } else {
+            testResults = 'not a palindrome.';
+        }
+        this.context.lwrWord = lwrWord;
+        this.context.revWord = revWord;
+        this.context.testResults = testResults;
+    }
 }
 
 register.viewControl('test-vc', TestViewControl);
